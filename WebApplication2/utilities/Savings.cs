10846796195a29026fb156e88023a908855bc1d8
Daniel Oyagha <daniel.oyagha@gmail.com>
2021-02-22 12:18:55 +0100

    all test finished passed

--- WebApplication2/utilities/Savings.cs
+++ WebApplication2/utilities/Savings.cs
@@ -11,37 +11,14 @@ namespace WebApplication2.utilities
 
        public decimal InterestCalculator(decimal p, decimal r, decimal t)
         {
-            decimal mySum = 0;
-       
-
-            //[Theory]
-            //[InlineData(10000, 5, 1, 10500)]
-            //[InlineData(1, 5, 1, 1)]
-            //[InlineData(-20000, 10, 0.5, 0)]
-            //[InlineData(2000, 5, 1, 20500)]
-
-
+           
             if (p < 0)
-            {
-                mySum = (p * r * t / 100) + p;
-                return mySum * 0;
-            }
+                return 0;
 
-            
-            else if (p == 1 )
-            {
-                mySum = (p * r * t / 100) + p;
+            else if (p == 1)
                 return 1;
-            }
-            else
-          
-            {
-                mySum = (p * r * t / 100) + p;
-                
-            }
-            return mySum;
 
-
+            return  (p * r * t / 100) + p;
 
 
         }
